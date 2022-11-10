51013f85607c3a4a3db78bc9b723f89830ed37a6
Rounak-28 <95576871+Rounak-28@users.noreply.github.com>
2022-11-10 17:58:54 +0530

    minor change

--- components/RightPanelTrendingCard.jsx
+++ components/RightPanelTrendingCard.jsx
@@ -2,7 +2,7 @@ const RightPanelTrendingCard = () => {
   return (
     <div className="h-20 my-[3px]">
       <p className="text-sm text-[#71767b]">Trending in universe</p>
-      <p className="font-semibold cursor-pointer">#ElonMuskOP</p>
+      <p className="font-semibold cursor-pointer">#ThisIsHashTag</p>
       <p className="text-sm text-[#71767b]">69K Tweets</p>
     </div>
   );
